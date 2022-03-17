c28f3f659d6c7a12b8f473a411f8c6710c750058
Christopher Dazzo <dazzo.chris@gmail.com>
2022-03-17 12:38:35 -0500

    wip: nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+
+const Nav = styled.nav`
+    height: 60px;
+    background: red;
+`;
+
+const Logo = styled(Link)`
+    color: #fff;
+`;
+
+const MenuBars = styled.i`
+
+`;
+
+const NavMenu = styled.div`
+
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <Logo exact to='/'>ELIXR</Logo>
+        <MenuBars />
+        <NavMenu>
+        </NavMenu>
+        <h1>Navbar</h1>
+    </Nav>
+  )
+}
+
+export default Navbar
