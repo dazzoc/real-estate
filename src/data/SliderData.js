4fec4b9e96ffba014caa89a19d078dcc8556bd81
Christopher Dazzo <dazzo.chris@gmail.com>
2022-03-21 16:38:06 -0500

    wip: images

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -1,58 +1,58 @@
 export const SliderData = [
     {
-        title: 'Luxury Villa in Bali, Indonesia',
-        price: '$4,280,000',
+        title: 'House in Knoxville, Tennessee',
+        price: '$2,280,000',
         path: '/homes',
         label: 'View Home',
         image: ImageOne,
         alt: 'House'
     },
     {
-        title: 'House in San Jose, Costa Rica',
-        price: '$2,860,000',
+        title: 'House in Lyon, France',
+        price: '$5,860,000',
         path: '/homes',
         label: 'View Home',
         image: ImageTwo,
         alt: 'House'
     },
     {
-        title: 'House in Calabasas, Califonia',
-        price: '$8,420,000',
+        title: 'House in Stockholm, Sweeden',
+        price: '$1,420,000',
         path: '/homes',
         label: 'View Home',
         image: ImageThree,
         alt: 'House'
     },
     {
-        title: 'House in Houston, Texas',
-        price: '$6,660,000',
+        title: 'House in Bern, Switzerland',
+        price: '$3,660,000',
         path: '/homes',
         label: 'View Home',
-        image: ImageOne,
+        image: ImageFour,
         alt: 'House'
     }
 ];
 
 export const SliderDataTwo = [
     {
-        title: 'House in Las Vegas, Nevada',
-        price: '$8,980,000',
+        title: 'House in Vancouver BC, Canada',
+        price: '$2,980,000',
         path: '/homes',
         label: 'View Home',
         image: ImageFive,
         alt: 'House'
     },
     {
-        title: 'House in Houston, Texas',
-        price: '$3,460,000',
+        title: 'House in Brussels, Belgium',
+        price: '$5,460,000',
         path: '/homes',
         label: 'View Home',
         image: ImageSix,
         alt: 'House'
     },
     {
-        title: 'House in Alajuela, Costa Rica',
-        price: '$5,420,000',
+        title: 'House in Brighton, United Kingdom',
+        price: '$4,420,000',
         path: '/homes',
         label: 'View Home',
         image: ImageSeven,
