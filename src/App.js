96aa9b2541e617501bd2bf194094adb87bf5e675
Christopher Dazzo <dazzo.chris@gmail.com>
2022-03-21 15:30:55 -0500

    wip: hero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import GlobalStyle from './globalStyles';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <Hero />
     </>
   );
 }
