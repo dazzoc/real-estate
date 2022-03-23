156068b25d7915e6ba89ab20325634ceaccbacc8
Christopher Dazzo <dazzo.chris@gmail.com>
2022-03-23 09:28:17 -0500

    wip: dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,64 @@
+
+import { FaTimes } from "react-icons/fa";
+import {Link} from 'react-router-dom';
+import styled from "styled-components";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+
+const DropdownContainer = styled.div`
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+    background: #cd853f;
+    display: grid;
+    align-items: center;
+    top: 0;
+    left: 0;
+    transition: 0.3s ease-in-out;
+    opacity: 1;
+    top: 0;
+`;
+
+const Icon = styled.div`
+    position: absolute;
+    top: 1.2rem;
+    right: 1.5rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+`;
+
+const CloseIcon = styled(FaTimes)``
+const DropdownWrapper = styled.div``
+const DropdownMenu = styled.div``
+const DropdownLink = styled(Link)``
+const BtnWrap = styled.div``
+
+
+const Dropdown = () => {
+  return (
+    <DropdownContainer>
+        <Icon>
+            <CloseIcon size={40}/>
+        </Icon>
+        <DropdownWrapper>
+            <DropdownMenu>
+                {menuData.map((item, index) => {
+                    <DropdownLink to={item.link} key={index}>
+                        {item.title}
+                    </DropdownLink>
+                })}
+            </DropdownMenu>
+            <BtnWrap>
+                <Button primary='true' round='true' to='/contact'>
+                    Contact Us
+                </Button>
+            </BtnWrap>
+        </DropdownWrapper>
+    </DropdownContainer>
+  )
+}
+
+export default Dropdown
